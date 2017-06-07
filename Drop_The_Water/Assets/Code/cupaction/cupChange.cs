bea56742a3fe33da09e95e7296ccf143edfd058b
0220jjhh <0220jjhh@daum.net>
2017-06-07 13:47:35 +0900

    170607

---
+++ Drop_The_Water/Assets/Code/cupaction/cupChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cupChange : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		Vector3 direction = this.transform.position;
+		
+		if(other.gameObject.tag.Equals("waterdrop"))
+		{
+			Destroy(other.gameObject);
+			GameObject changecup = GameObject.Find("cup_2");
+			changecup.transform.position = direction;
+			Destroy(this.gameObject);
+		}
+	}
+}
